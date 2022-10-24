00b30d420f479e70017db2d268afb657c9046c7b
Gasiorek, Piotr <piotrgasiorek773@gmail.com>
2022-10-24 19:47:49 +0200

    Day 2 - 23.10.2022

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const http = require("http");
 
-const server = http.createServer((request, response) => {
-  if (request.url === "/") {
-    response.end("Welcome to our page");
-  }
-  if (request.url === "/about") {
-    response.end("About page");
-  } else {
-    response.end(
-      `<h1>Ooops!</h1>
-        <p>We can't seem to find a page you are looking for</p>
-        <a href="/">go back to home page</a>`
-    );
-  }
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+const server = http.createServer();
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on("request", (req, res) => {
+  res.end("Welcome");
 });
 
 server.listen(5000);
