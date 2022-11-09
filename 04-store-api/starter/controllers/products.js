0575552dec139b3de75285c56116442249699583
Gasiorek, Piotr <piotrgasiorek773@gmail.com>
2022-11-09 23:29:58 +0100

    Day 8 9.11.2022

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,24 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({});
+  res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products });
 };
 
 module.exports = {
