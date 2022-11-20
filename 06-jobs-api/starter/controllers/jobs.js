4febf77f327c0969bd8a6c9999ca936d2fe4356c
Gasiorek, Piotr <piotrgasiorek773@gmail.com>
2022-11-20 15:33:23 +0100

    Day 10, 11, 12 11-20.11

---
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const getAllJobs = async (req, res) => {
+  res.send("getAllJobs");
+};
+
+const getJob = async (req, res) => {
+  res.send("getJob");
+};
+
+const createJob = async (req, res) => {
+  res.send(req.user);
+};
+
+const updateJob = async (req, res) => {
+  res.send("updateJob");
+};
+
+const deleteJob = async (req, res) => {
+  res.send("deleteJob");
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
